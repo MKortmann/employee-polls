818d3058eeca77f3aa32617b6ac1075852764fcc
MKortmann <companymvms@gmail.com>
2022-11-18 16:02:53 +0100

    add: Leaderboard users ordered data

--- src/components/pages/Leaderboard.tsx
+++ src/components/pages/Leaderboard.tsx
@@ -2,39 +2,58 @@ import React from 'react'
 
 import Table from 'react-bootstrap/Table'
 import Container from 'react-bootstrap/Container'
+import Figure from 'react-bootstrap/Figure'
+
+import { getUsers, getAvatar } from '../../redux/slices/usersSlice'
+import { useSelector } from 'react-redux'
+import type { AppDispatch } from '../../redux/store'
 
 interface Props {}
 
 export const Leaderboard: React.FC<Props> = () => {
+	const users = useSelector(getUsers)
+
+	const sortUsers = Object.entries(users)
+		.sort((u1: any, u2: any) => {
+			return (
+				Object.keys(u1[1].answers).length +
+				u1[1].questions.length -
+				(Object.keys(u2[1].answers).length + u2[1].questions.length)
+			)
+		})
+		.reverse()
+
 	return (
 		<Container>
 			<Table striped bordered hover className='my-5'>
 				<thead>
 					<tr>
 						<th>#</th>
+						<th>Avatar</th>
 						<th>Users</th>
 						<th>Answered</th>
 						<th>Created</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>1</td>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<td>3</td>
-						<td colSpan={2}>Larry the Bird</td>
-						<td>@twitter</td>
-					</tr>
+					{sortUsers.map((user: any, index) => (
+						<tr>
+							<td>{index}</td>
+							<td>
+								<Figure>
+									<Figure.Image
+										width={36}
+										height={24}
+										alt='vatar'
+										src={getAvatar[user[0]]}
+									/>
+								</Figure>
+							</td>
+							<td>{user[1].name}</td>
+							<td>{Object.keys(user[1]?.answers)?.length}</td>
+							<td>{user[1]?.questions.length}</td>
+						</tr>
+					))}
 				</tbody>
 			</Table>
 		</Container>
